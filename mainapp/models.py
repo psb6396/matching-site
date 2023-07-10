f45df783b27ff0c1470ebe0299a674e256ae441d
psb6396 <psb6396@naver.com>
2023-07-10 15:33:54 +0900

    ㅁㄴㅇㄹ

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Player(AbstractUser):
     score = models.IntegerField(default = 1000)
     intention_to_fight = models.BooleanField(default = False)
-    matching_request_from_others = models.BooleanField(default=False)    
+    matching_request_from_others = models.BooleanField(default=False) 
+    # 매칭 요청(누가 했는지에 대한 정보는 어떡하지??)   
     is_refree = models.BooleanField(default=False)
 
 class Referee(models.Model):
