8036f142394ed13e2c8380b6f049aec0b5929699
psb6396 <psb6396@naver.com>
2023-09-01 00:26:13 +0900

    asdf

--- mainapp/views.py
+++ mainapp/views.py
@@ -79,31 +79,28 @@ def match_make(request):
     else:
         return render(request, 'mainapp/gym_time.html', context)
     
-#매칭 잡기 함수
 @login_required
-def match_request(request, match_id):
+def match_request_page(request):
     # matches = Match.objects.all().order_by('date', 'time') 어떻게 렌더링 될지 모르니깐
     matches = Match.objects.all().order_by('date')
     context = {'matches' : matches}
-    
-    if request.method == 'GET':
-        me = request.user
-        player1 = My_user.objects.get(pk = me.id)
-        player1.intention_to_fight = True
-        random_opponent_player = My_user.objects.exclude(Q(pk = player1.id) | Q(intention_to_fight = False))
-        repetition_confirm = player1.match_set.get(pk = match_id)          # player1의 match를 현재의 match와 중복확인.
-        if(repetition_confirm != None):
+    return render(request, 'mainapp/match_request.html', context) 
+
+def match_request(request, match_id):
+    me = request.user
+    player1 = My_user.objects.get(pk = me.id)
+    player1.intention_to_fight = True
+    random_opponent_player = My_user.objects.exclude(Q(pk = player1.id) | Q(intention_to_fight = False))
+    repetition_confirm = player1.match_set.get(pk = match_id)          # player1의 match를 현재의 match와 중복확인.
+    if (repetition_confirm != None):
             
-            if (random_opponent_player != None):
+        if (random_opponent_player != None):
             
-                player2 = random.choice(random_opponent_player)
-                chosen_match = Match(pk = match_id)
-                chosen_match.player.add(player1, player2)
-                chosen_match.save()
+            player2 = random.choice(random_opponent_player)
+            chosen_match = Match(pk = match_id)
+            chosen_match.player.add(player1, player2)
+            chosen_match.save()    
             
-    else:
-        return render(request, 'mainapp/match_request.html', context) 
-        #상대없으면 상대 없다는 메세지 띄우고 홈페이지 돌아가기
 
 # def define_winner(request): #
 #     me = request.user
