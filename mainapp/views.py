af022dc93005bf19f53bda358bda4ed2308d7829
psb6396 <psb6396@naver.com>
2023-09-04 16:30:12 +0900

    still stuck

--- mainapp/views.py
+++ mainapp/views.py
@@ -94,9 +94,17 @@ def match_request(request, match_id):
     random_opponent_player = My_user.objects.exclude(Q(pk = player1.id) | Q(intention_to_fight = False))
     
     # 클릭한 사람의 경기중에 클릭한 시간대와 동일한 경기가 있으면 오류발생 시키고 싶음.
-    if Match.objects.filter(pk = match_id, player = me)
-
-
+    if Match.objects.filter(Q(pk = match_id) & Q(player = me)).exists():
+        return render()
+    elif (random_opponent_player != None):
+        player2 = random.choice(random_opponent_player)
+        chosen_match = Match(pk = match_id)
+        chosen_match.player.add(player1, player2)
+        chosen_match.save()
+        return redirect('mainapp:match_request_page')     
+    
+    
+    
     # 아래는 실패한 코드
     # try:
     #     player1.player_match.get(pk = match_id)
